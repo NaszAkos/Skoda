888f71064eb1e223ee345532ca26dcc7e79ddc96
Adolf Áron <adolfaron@turr.hu>
2025-09-17 09:20:35 +0200

    hover javítások

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 let fejlec_meret = 72;
-let lastScrollTop = 0;
+let utolsoGorgetes = 0;
 const navbar = document.getElementById("fejlec");
 window.addEventListener("scroll", function() {//görgrtésellenőrzés, fejléc animáció
     navbar.style.transition = `top ${fejlec_meret/200}s ease-in-out, box-shadow ${fejlec_meret/200}s ease-in-out`;
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;//visszadja a görgetés helyzetét újabb||régebbi
+    let aktuaklisGorgetes = window.pageYOffset || document.documentElement.scrollTop;//visszadja a görgetés helyzetét újabb||régebbi
 
-    if (currentScroll > lastScrollTop) {
+    if (aktuaklisGorgetes > utolsoGorgetes) {
         navbar.style.top = -fejlec_meret + "px";
-        if (currentScroll > fejlec_meret) {
+        if (aktuaklisGorgetes > fejlec_meret) {
             navbar.style.boxShadow = "none"
         } else {
             navbar.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)"
         }
     } else {
         navbar.style.top = "0";
-        if (currentScroll > 0) {
+        if (aktuaklisGorgetes > 0) {
             navbar.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)"
             
         } else {
@@ -22,5 +22,5 @@ window.addEventListener("scroll", function() {//görgrtésellenőrzés, fejléc 
             navbar.style.boxShadow = "none"
         }
     }
-    lastScrollTop = currentScroll;
+    utolsoGorgetes = aktuaklisGorgetes;
 });
